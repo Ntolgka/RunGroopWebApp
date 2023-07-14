53f4a752dc562f1fd2a0268fc82cc0795e8041d8
Ntolgka <nalbanttolga1@gmail.com>
2023-07-14 12:37:40 +0300

    Initial commit

---
+++ RunGroopWebApp/Models/AppUserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RunGroopWebApp.Models
+{
+    public class AppUserModel
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public AddressModel? Address { get; set; }
+        public ICollection<ClubModel>? Clubs { get; set; }
+        public ICollection<RacesModel> Races { get; set; }
+
+    }
+}
