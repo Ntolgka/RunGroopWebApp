cefd83168a5faf5f21ae04a1a7c102c8c0b1685d
Ntolgka <nalbanttolga1@gmail.com>
2023-07-18 12:58:43 +0300

    Add View of Club and Race

---
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+          
+    }
+}
