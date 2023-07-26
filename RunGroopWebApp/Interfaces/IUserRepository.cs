7ab18248203d87747614a6f1e485792b313a9014
Ntolgka <nalbanttolga1@gmail.com>
2023-07-26 18:01:26 +0300

    Add User Index and Detail page

---
+++ RunGroopWebApp/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<AppUserModel>> GetAllUsers();
+        Task<AppUserModel> GetUserById(string id);
+        bool Add(AppUserModel user);
+        bool Update(AppUserModel user);
+        bool Delete(AppUserModel user);
+        bool Save();
+    }
+}
