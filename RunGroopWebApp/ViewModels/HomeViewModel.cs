22092b1eaf21dca8f33393fa3782ce472bc085f4
Ntolgka <nalbanttolga1@gmail.com>
2023-07-27 19:52:18 +0300

    Add IP Geo Location

---
+++ RunGroopWebApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<ClubModel> Clubs { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
